fa39c2fc35f6107af0b631bb1c9091a52f7a502f
Alex Kirhenshtein <alk@netxms.org>
2023-02-01 15:24:48 +0200

    FC36/37 added to targets

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,7 @@ set -e
 
 mock -r rocky+epel-8-$(arch) --spec SPECS/*.spec --sources SOURCES
 mock -r rocky+epel-9-$(arch) --spec SPECS/*.spec --sources SOURCES
+mock -r fedora-36-$(arch) --spec SPECS/*.spec --sources SOURCES
+mock -r fedora-37-$(arch) --spec SPECS/*.spec --sources SOURCES
+
 cp /var/lib/mock/*/result/*.rpm /result/
