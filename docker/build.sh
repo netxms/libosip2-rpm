551cdea2af7d030f6b4433b2fe1314f2e6e43d03
Alex Kirhenshtein <alk@netxms.org>
2024-12-27 13:56:36 +0200

    Amazon Linux added; FC up to v40 removed

--- docker/build.sh
+++ docker/build.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+for V in 2 2023; do
+   mock -r amazonlinux-$V-$(arch) --spec SPECS/*.spec --sources SOURCES
+done
+
 for V in 8 9; do
    mock -r rocky+epel-$V-$(arch) --spec SPECS/*.spec --sources SOURCES
 done
 
-for V in 37 38 39; do
+for V in 40 41; do
    mock -r fedora-$V-$(arch) --spec SPECS/*.spec --sources SOURCES
 done
 
